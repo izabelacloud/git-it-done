3738f424526987af0497807b065eb6fe08c8bc57
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-04-13 21:55:08 -0700

    changes

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,21 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username")
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        //to get the repos with username
+        getUserRepos(username);
+        //to clear the input form field after submit
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username")
+    }
+    console.log(event);
+}
+
 var getUserRepos = function(user) {
     //format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -8,18 +26,11 @@ var getUserRepos = function(user) {
             console.log(data);
         })
     })
-    // console.log("function was called");
-    // fetch("https://api.github.com/users/octocat/repos").then(function(response) {
-    //     response.json().then(function(data) {
-    //         console.log(data);
-    //     })
-    //     console.log("inside", response);
-    // })
 
-    // console.log("outside");
-
-    // var response = fetch("https://api.github.com/users/octocat/repos");
-    // console.log(response);
 }
 
-getUserRepos("izabelacloud");
+
+
+// getUserRepos();
+userFormEl.addEventListener("submit", formSubmitHandler);
+
