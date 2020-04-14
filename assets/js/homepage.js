bad4f61d42a7d545bad6322da1a7ac64f9e6373c
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-04-13 21:17:32 -0700

    initial call to server

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,25 @@
-var getUserRepos = function() {
-    console.log("function was called");
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(user) {
+    //format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    //make request to the URL
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        })
+    })
+    // console.log("function was called");
+    // fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+    //     response.json().then(function(data) {
+    //         console.log(data);
+    //     })
+    //     console.log("inside", response);
+    // })
+
+    // console.log("outside");
+
+    // var response = fetch("https://api.github.com/users/octocat/repos");
+    // console.log(response);
 }
 
-getUserRepos();
+getUserRepos("izabelacloud");
